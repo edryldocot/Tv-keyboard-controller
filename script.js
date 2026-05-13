f1727fddcd8f2f05126d622d2fe6c951d44b4ee2
edryldocot <edrylbajaladocot11@gmail.com>
2026-05-13 16:11:25 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 let bluetoothDevice, server, characteristic;
 let lastX = 0, lastY = 0;
 
-// STANDARD BLUETOOTH HID - G
+// 📡 BLUETOOTH CONFIG
+const HID_SERVICE_UUID = "00001812-0000-1000-8000-00805f9b34fb";
+const HID_REPORT_UUID  = "00002a4d-0000-1000-8000-00805f9b34fb";
 
+// ===============================
+// 🔘 SWITCH VIEW 1 & 2
+// ===============================
+const btn1 = document.getElementById('btn-view1');
+const btn2 = document.getElementById('btn-view2');
+const mouseArea = document.getElementById('mouse-area');
+const optionsPanel = document.getElementById('options-panel');
 
+btn1.addEventListener('click', () => {
+    btn1.classList.add('active');
+    btn2.classList.remove('active');
+    mouseArea.classList.add('mouse-hidden');
+    optionsPanel.classList.add('options-hidden');
+});
 
-
+btn2.addEventListener('click', () => {
+    btn2.classList.add('active');
+    btn1.classList.remove('active');
+    mouseArea.classList.remove('
